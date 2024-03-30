70581e3a47a54210d94b15a49a9b320f89c29b3a
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2024-03-30 12:05:43 +0530

    Refactored code by adding service

--- src/main/java/ai/rudra/employee_management/controllers/EmployeeController.java
+++ src/main/java/ai/rudra/employee_management/controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package ai.rudra.employee_management.controllers;
 
 import ai.rudra.employee_management.dao.EmployeeDao;
 import ai.rudra.employee_management.entities.Employee;
+import ai.rudra.employee_management.services.EmployeeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,48 +18,43 @@ public class EmployeeController {
 //    Load the data of all the employees for easy response
 
     List<Employee> employeeList;
-    private final EmployeeDao employeeDao;
 
-    @Autowired
-    public EmployeeController(EmployeeDao employeeDao){
-        this.employeeDao = employeeDao;
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
     }
-
-    @PostConstruct
-    public void getAllEmployees(){
-        employeeList = employeeDao.getAllEmployees();
-    }
-
 //    Get all the employees
     @GetMapping("/employees")
     public ResponseEntity<List<Employee>> allEmployees(){
+        employeeList = employeeService.getAllEmployees();
         return new ResponseEntity<>(employeeList, HttpStatus.FOUND);
     }
 
 //    Get a single employee
     @GetMapping("/employees/{employeeId}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable int employeeId){
-        return new ResponseEntity<>(employeeDao.getEmployeeById(employeeId), HttpStatus.FOUND);
+        return new ResponseEntity<>(employeeService.getEmployeeById(employeeId), HttpStatus.FOUND);
     }
 
 //    Create a new employee
     @PostMapping("/employees")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
-        Employee emp = employeeDao.createEmployee(employee);
+        Employee emp = employeeService.createEmployee(employee);
         return new ResponseEntity<>(emp,HttpStatus.CREATED);
     }
 
 //    Update an employee
     @PutMapping("/employees/{employeeId}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable int employeeId, @RequestBody Employee employee){
-        Employee updateEmployee = employeeDao.updateEmployee(employeeId, employee);
+        Employee updateEmployee = employeeService.updateEmployee(employeeId, employee);
         return new ResponseEntity<>(updateEmployee, HttpStatus.CREATED);
     }
 
 //    Delete an employee
     @DeleteMapping("/employees/{employeeId}")
     public ResponseEntity<String> deleteEmployee(@PathVariable int employeeId){
-        employeeDao.deleteEmployee(employeeId);
+        employeeService.deleteEmployee(employeeId);
         return new ResponseEntity<>("Employee Deleted Successfully", HttpStatus.OK);
     }
 
