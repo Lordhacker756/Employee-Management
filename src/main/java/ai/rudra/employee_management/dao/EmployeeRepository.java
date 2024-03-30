1b6551680ed4fefebe00175ad84f2218e539934d
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2024-03-30 14:11:58 +0530

    Converted to spring data rest
    We now no longer need Controllers, Dao, and Services as these are now automatically managed along with pagination and sorting

---
+++ src/main/java/ai/rudra/employee_management/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package ai.rudra.employee_management.dao;
+
+import ai.rudra.employee_management.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
